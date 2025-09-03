46bbbae40144a7c723147a9f0b44063ca733e7d6
Rafael Costa <rafaelcost@gmail.com>
2025-09-03 09:57:57 -0300

    1

---
+++ src/main/java/com/senai/projeto_escola/application/dto/AlunoDTO.java
@@ -0,0 +1,31 @@
+package com.senai.projeto_escola.application.dto;
+
+import com.senai.projeto_escola.domain.entity.Aluno;
+import com.senai.projeto_escola.domain.entity.Curso;
+
+public record AlunoDTO(
+        String nome,
+        Long cpf,
+        String idCurso,
+        String turma
+) {
+    public static AlunoDTO fromEntity(Aluno aluno){
+        if(aluno ==null) return null;
+        return new AlunoDTO(
+                aluno.getNome(),
+                aluno.getCpf(),
+                aluno.getCurso() != null ? aluno.getCurso().getId() : null,
+                aluno.getTurma()
+        );
+    }
+
+    public Aluno toEntity (Curso curso){
+        Aluno aluno = new Aluno();
+        aluno.setNome(this.nome);
+        aluno.setCpf(this.cpf);
+        aluno.setTipo("Aluno");
+        aluno.setTurma(this.turma);
+        aluno.setCurso(curso);
+        return aluno;
+    }
+}
